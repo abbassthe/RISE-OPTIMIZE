220695976c0aa5f4e22b1d5078fb385ad773e95b
Mohamad Raslan <Refez.25@outlook.com>
2024-07-11 15:29:44 +0400

    Adding map component to the website

--- RISE/src/App.tsx
+++ RISE/src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/navbar/Navbar";
 import Contact from "./Components/Contact/Contact";
 import About from "./Components/About/About";
+import About2 from "./Components/About2/About2";
+import Vid from "./Components/Vid/Vid";
+import Vid2 from "./Components/Vid2/Vid";
+import FibonacciDiv from "./Components/test/tst";
 import "./App.css";
+import Globe from "./Components/Globe/Globe";
+import Model from "./Components/Locust/Model";
+import Scene from "./Components/Locust/Scene";
+import Map from "./Components/Map/Map";
 
 function App() {
   return (
@@ -14,8 +19,20 @@ function App() {
         <section>
           <Navbar />
           <Home />
+          <Globe />
+          <div className="t">
+            <span id="txt1">HELLO</span>
+            <span id="txt2">World</span>
+            <div className="testWrap">
+              <Scene />
+            </div>
+          </div>
+          <Vid />
           <About />
+          <About2 />
+          <Vid2 />
           <Contact />
+ 
         </section>
       </div>
     </>
--- RISE/src/Components/Login_SignUp/Create_Account.tsx
+++ RISE/src/Components/Login_SignUp/Create_Account.tsx
@@ -1,3 +1,4 @@
+@ -1,349 +0,0 @@
 import React, { useState } from 'react';
 import {
     View,
