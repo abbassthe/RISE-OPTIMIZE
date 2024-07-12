f000e46e7dfd2436724c4b0f9c70920e3155c5c6
abbassthe <47321004+abbassthe@users.noreply.github.com>
2024-07-13 00:09:42 +0300

    fixed files

--- Project/RISE/src/main.tsx
+++ Project/RISE/src/main.tsx
@@ -1,23 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Map from "./Components/Map/Map";
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Map from "./Components/Map/Map.tsx";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-            <Router> 
-            <Routes>
-            <Route path="/homepage" element={<App />} /> 
-            <Route path="/" element={<App />} /> 
-            <Route path="/map" element={<Map />} /> 
-
-            </Routes>
-  
+    <Router>
+      <Routes>
+        <Route path="/homepage" element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
